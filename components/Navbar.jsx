732b98b6c2755edff4e540fdd067d6f7e3354cb5
omar-bay <omarmb2002@gmail.com>
2022-03-26 15:54:36 +0200

    first commit, working on Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import styles from '../styles/Navbar.module.css'
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            {/* cred */}
+            <div className={styles.item}>
+                <div className={styles.callButton}>
+                    <Image src="/img/phone.png" alt="call" width="32" height="32"/>
+                </div>
+                <div className={styles.texts}>
+                    <div className={styles.text}>ORDER NOW</div>
+                    <div className={styles.text}>71 239 812</div>
+                </div>
+            </div>
+
+            {/* menu items */}
+            <div className={styles.item}>
+                <ul className={styles.list}>
+                    <li className={styles.listItem}>Home</li>
+                    <li className={styles.listItem}>Products</li>
+                    <li className={styles.listItem}>Menu</li>
+                    <Image src="/img/logo.png" alt="logo" width="100px" height="80px"/>
+                    <li className={styles.listItem}>Events</li>
+                    <li className={styles.listItem}>Blog</li>
+                    <li className={styles.listItem}>Contact</li>
+                </ul>
+            </div>
+
+            {/* cart button */}
+            <div className={styles.item}>
+                <div className={styles.cart}>
+                    <Image src="/img/cart.png" alt="logo" width="40px" height="40px"/>
+                </div>
+                <div className={styles.counter}>2</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Resturant</title>
+        <meta name="description" content="Perfection in our Pizza" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      homepage
+    </div>
+  )
+}
