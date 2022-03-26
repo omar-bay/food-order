86511dc4873b36f827337592413098395cd571e2
omar-bay <omarmb2002@gmail.com>
2022-03-26 23:24:03 +0200

    PizzaCard UI done

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,9 +1,22 @@
+import Image from 'next/image'
 import styles from '../styles/PizzaCard.module.css'
 
 const PizzaCard = () => {
     return (
         <div className={styles.container}>
-            Pizza
+            {/* Image */}
+            <Image src="/img/pizza.png" alt="" width="500" height="500"/>
+            <h1 className={styles.title}>Pepperoni Bue Bay</h1>
+            <span className={styles.price}>$19.90</span>
+            <p className={styles.desc}>
+                LauriPatterson was the first Finder, and we are his
+                descenders.
+            </p>
+
+            {/* Description */}
+
+            {/* Title */}
+
         </div>
     )
 }
