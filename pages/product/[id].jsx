682b83f0c5dab5bbab2783125b8b39228f4032e0
omar-bay <omarmb2002@gmail.com>
2022-03-27 19:38:06 +0300

    Product page UI done

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -43,6 +43,48 @@ const Product = () => {
                     </div>
                 </div>
                 <h3 className={styles.choose}>Choose additional ingredients</h3>
+                <div className={styles.ingredients}>
+                    <div className={styles.option}>
+                        <input
+                        type="checkbox"
+                        id="double"
+                        name="double"
+                        className={styles.checkbox}
+                        />
+                        <label htmlFor='double'>Double Ingredients</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input
+                        type="checkbox"
+                        id="cheese"
+                        name="cheese"
+                        className={styles.checkbox}
+                        />
+                        <label htmlFor='cheese'>Extra Cheese</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input
+                        type="checkbox"
+                        id="spicy"
+                        name="spicy"
+                        className={styles.checkbox}
+                        />
+                        <label htmlFor='spicy'>Hot Sauce</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input
+                        type="checkbox"
+                        id="garlic"
+                        name="garlic"
+                        className={styles.checkbox}
+                        />
+                        <label htmlFor='garlic'>Garlic Sauce</label>
+                    </div>
+                </div>
+                <div className={styles.add}>
+                    <input type="number" defaultValue={1} className={styles.quantity}/>
+                    <button className={styles.button}>Add to Cart</button>
+                </div>
             </div>
 
         </div>
