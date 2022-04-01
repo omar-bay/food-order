c344a34cbfa220204fdf45abca873cb847a9c9dc
omar-bay <omarmb2002@gmail.com>
2022-04-01 15:51:35 +0300

    working on 2nd row orders left

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -2,9 +2,74 @@ import styles from '../../styles/Order.module.css'
 import Image from 'next/image'
 
 const Order = () => {
+    const status = 0
+
+    const statusClass = (index) => {
+        if(index-status < 1) return styles.done
+        if(index-status === 1) return styles.inProgress
+        if(index-status > 1) return styles.undone
+    }
+
     return (
         <div className={styles.container}>
-            <div className={styles.left}></div>
+            <div className={styles.left}>
+                <div className={styles.row}>
+                    <table className={styles.table}>
+                        <tr className={styles.tr}>
+                            <th>Order Id</th>
+                            <th>Customer</th>
+                            <th>Address</th>
+                            <th>Total</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span className={styles.id}>254452</span>
+                            </td>
+                            <td>
+                                <span className={styles.name}>
+                                    Elena Gilbert
+                                </span>
+                            </td>
+                            <td>
+                                <span className={styles.address}>6118 N Shiloh Rd</span>
+                            </td>
+                            <td>
+                                <span className={styles.total}>$39.9</span>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div className={styles.row}>
+                    <div className={statusClass(0)}>
+                        <Image src="/img/paid.png" width={30} height={30} alt=""/>
+                        <span>Payment</span>
+                        <div className={styles.checkedIcon}>
+                            <Image src="/img/checked.png" width={20} height={20} alt=""/>
+                        </div>
+                    </div>
+                    <div className={statusClass(1)}>
+                        <Image src="/img/bake.png" width={30} height={30} alt=""/>
+                        <span>Preparing</span>
+                        <div className={styles.checkedIcon}>
+                            <Image src="/img/checked.png" width={20} height={20} alt=""/>
+                        </div>
+                    </div>
+                    <div className={statusClass(2)}>
+                        <Image src="/img/bike.png" width={30} height={30} alt=""/>
+                        <span>On the Way</span>
+                        <div className={styles.checkedIcon}>
+                            <Image src="/img/checked.png" width={20} height={20} alt=""/>
+                        </div>
+                    </div>
+                    <div className={statusClass(3)}>
+                        <Image src="/img/delivered.png" width={30} height={30} alt=""/>
+                        <span>Delivered</span>
+                        <div className={styles.checkedIcon}>
+                            <Image src="/img/checked.png" width={20} height={20} alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div className={styles.right}>
                 <div className={styles.wrapper}>
