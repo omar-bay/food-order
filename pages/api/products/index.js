405cd15eb59afd833980686d63b95baf3465f0bc
omar-bay <omarmb2002@gmail.com>
2022-04-04 20:16:27 +0300

    modified:   pages/api/products/index.js

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,6 +1,12 @@
-export default function handler(req, res) {
+import dbConnect from "../../../util/mongo"
+import Product from '../../../models/Product'
+
+export default async function handler(req, res) {
     const { method } = req;
 
+    dbConnect()
+
     if (method === "GET") {}
+
     if (method == "POST") {}
 }
